27c575da17774661dee2cbfa7c7f6076f6e63813
DESKTOP-G3E1MO0\ScorpionIPX <scorpionipx@gmail.com>
2017-02-18 16:14:00 +0200

    Created project in Atmel Studio

---
+++ SmartFlowerpot/SmartFlowerpot/main.c
@@ -0,0 +1,18 @@
+/*
+ * SmartFlowerpot.c
+ *
+ * Created: 2/18/2017 4:13:13 PM
+ * Author : ScorpionIPX
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
