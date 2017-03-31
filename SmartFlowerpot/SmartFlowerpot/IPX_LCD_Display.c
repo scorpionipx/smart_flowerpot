b9ed933f76023bf6c95fa568c33f630b8d1ee435
DESKTOP-G3E1MO0\ScorpionIPX <scorpionipx@gmail.com>
2017-03-31 23:09:16 +0300

    Ready to develop branches

--- SmartFlowerpot/SmartFlowerpot/IPX_LCD_Display.c
+++ SmartFlowerpot/SmartFlowerpot/IPX_LCD_Display.c
@@ -9,13 +9,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <string.h>
-#include "IPX_ADC.h"
 
-void init_Strings(void)
-{
-	strcpy(authors,"                Nicolae-Danut Popa                 ");
-	strcpy(info,"               Controlul motorului pas-cu-pas                 ");
-}
 
 void put_Char_LCD_Display(int ch, int type)
 {
@@ -34,7 +28,6 @@ void put_Char_LCD_Display(int ch, int type)
 void init_LCD_Display(void)
 {
 	DDRC = 0xFF;
-	init_Strings();
 	put_Char_LCD_Display(0x03,0);
 	_delay_ms(5);
 	put_Char_LCD_Display(0x03,0);
@@ -50,65 +43,4 @@ void init_LCD_Display(void)
 	put_Char_LCD_Display(0x80,0);
 	_delay_ms(5);
 	put_Char_LCD_Display(0x01,0);//clear display
-}
-
-void display_Info(void)
-{
-	put_Char_LCD_Display(0xC0,0);
-	for(int i = 0;i < 16; i ++)
-	{
-		put_Char_LCD_Display(info[i + ch_index],1);
-	}
-	ch_index ++;
-	if (ch_index >= 47)
-	{
-		ch_index = 0;
-	}
-	_delay_ms(250);
-}
-
-void display_Authors(void)
-{
-	put_Char_LCD_Display(0xC0,0);
-	for(int i = 0;i < 16; i ++)
-	{
-		put_Char_LCD_Display(authors[i + ch_index],1);
-	}
-	ch_index ++;
-	if (ch_index >= 35)
-	{
-		ch_index = 0;
-	}
-	_delay_ms(250);
-}
-
-void display_Duty_Cycle_L(void)
-{
-	unsigned int percentage = OCR1A/8;
-	put_Char_LCD_Display(0xCC,0);
-	put_Char_LCD_Display((percentage/100)%10 + 48,1);
-	put_Char_LCD_Display((percentage/10)%10 + 48,1);
-	put_Char_LCD_Display(percentage%10 + 48,1);
-	_delay_ms(500);
-}
-
-void display_Duty_Cycle_R(void)
-{
-	unsigned int percentage = OCR1B/8;
-	put_Char_LCD_Display(0xCC,0);
-	put_Char_LCD_Display((percentage/100)%10 + 48,1);
-	put_Char_LCD_Display((percentage/10)%10 + 48,1);
-	put_Char_LCD_Display(percentage%10 + 48,1);
-	_delay_ms(500);
-}
-
-void display_Speed(void)
-{
-	int voltage_mv = Get_ADC_Voltage();
-	put_Char_LCD_Display(0xCB,0);
-	put_Char_LCD_Display((voltage_mv/1000)%10 + 48,1);
-	put_Char_LCD_Display((voltage_mv/100)%10 + 48,1);
-	put_Char_LCD_Display((voltage_mv/10)%10 + 48,1);
-	put_Char_LCD_Display(voltage_mv%10 + 48,1);
-	_delay_ms(500);
 }
--- SmartFlowerpot/SmartFlowerpot/main.c
+++ SmartFlowerpot/SmartFlowerpot/main.c
@@ -5,12 +5,13 @@
  * Author : ScorpionIPX
  */ 
 
+#include "Global.h"
 #include <avr/io.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
+	
     while (1) 
     {
     }
