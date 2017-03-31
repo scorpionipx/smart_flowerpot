273d94aff7750c96c4b6624f516c06f874114c3c
DESKTOP-G3E1MO0\ScorpionIPX <scorpionipx@gmail.com>
2017-03-31 23:02:01 +0300

    Constants

--- SmartFlowerpot/SmartFlowerpot/Global.h
+++ SmartFlowerpot/SmartFlowerpot/Global.h
@@ -8,6 +8,14 @@
 /*============================================================================*/
 #define F_CPU		16000000L //clock freq-must be define before including util/delay.h
 
+// Soil moisture level threshold for sensor no. 1 when water pump should be turned on 
+#define SOIL_MOISTURE_LOW_LEVEL_THRESHOLD_S1 30
+// Soil moisture level threshold for sensor no. 2 when water pump should be turned on
+#define SOIL_MOISTURE_LOW_LEVEL_THRESHOLD_S2 30
+// Average soil moisture level threshold when water pump should be turned on
+#define SOIL_MOISTURE_LOW_LEVEL_THRESHOLD_AVERAGE 35
+
+
 typedef signed char sint8;
 typedef unsigned char uint8;
 typedef signed int sint16;
